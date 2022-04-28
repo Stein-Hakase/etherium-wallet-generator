02bc321f7c4f90617f19d5960c4e0c5c76495cf6
Fakhreddine Hariz <Fakhriddine.hariz@gmail.com>
2022-04-28 18:17:01 +0700

    change to random generation

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 var ethers = require('ethers');
-var crypto = require('crypto');
+// var crypto = require('crypto');
 
-var id = crypto.randomBytes(32).toString('hex');
-var privateKey = "0x" + id;
-console.log("SAVE BUT DO NOT SHARE THIS:", privateKey);
+// var id = crypto.randomBytes(32).toString('hex');
+// var privateKey = "0x" + id;
+// console.log("SAVE BUT DO NOT SHARE THIS:", privateKey);
 
-var wallet = new ethers.Wallet(privateKey);
+// var wallet = new ethers.Wallet(privateKey);
+
+const wallet = ethers.Wallet.createRandom()
+console.log('privateKey:', wallet.privateKey)
+console.log('mnemonic:', wallet.mnemonic.phrase)
 console.log("Address: " + wallet.address);
 console.log("Publickey : " + wallet.publicKey)
